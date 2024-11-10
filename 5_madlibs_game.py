390f47ee3fe04ebb2a402cb7964f65127aefb324
Demegorash <felipe.solisgranados@gmail.com>
2024-11-10 17:41:53 -0600

    Python_Course_Practice2024_00to05

---
+++ 5_madlibs_game.py
@@ -0,0 +1,13 @@
+# Madlibs game.  This is an exercise by doing a game.  The objective of the game is to create a story by filling blanks random words.
+
+adjective1 = input("Enter and adjective (description): ")
+noun1 = input("Enter a noun (person, place, thing): ")
+adjective2 = input("Enter and adjective (description): ")
+verb1 = input("Enter a verb ending with 'ing': ")
+adjective3 = input("Enter and adjective (description): ")
+
+
+print(f"Today I went to a {adjective1} zoo.")
+print(f"In a exhibit, I saw a {noun1}.")
+print(f"{noun1} was {adjective2} and {verb1}.")
+print(f"I was {adjective3}!")
