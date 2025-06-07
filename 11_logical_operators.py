1439105b9297239e3416ed6ef33334b4f920565c
Demegorash <felipe.solisgranados@gmail.com>
2025-06-06 21:13:02 -0600

    Learning Python with BroCode from YouTube. Lessons 1-12

---
+++ 11_logical_operators.py
@@ -0,0 +1,52 @@
+""" 
+Remove comments in order to run each part of the program = or
+
+temp = 36
+is_rainning = False
+
+if temp > 35 or temp < 0 or is_rainning:
+    print("The outdoor event is cancelled")
+else:
+    print("The outdoor event is still scheduled")
+"""    
+
+"""
+Remove comments in order to run each part of the program = and
+
+temp = 20
+is_sunny = True
+
+if temp >= 28 and is_sunny:
+    print("It is HOT outside")
+    print("It is SUNNY")
+elif temp <= 0 and is_sunny:
+    print("It is COLD outside")
+    print("Is is SUNNY")
+elif 28 > temp > 0 and is_sunny:
+    print("It is WARM outside")
+    print("It is SUNNY")
+"""
+
+# Remove comments in order to run each part of the program = not
+
+temp = 0
+is_sunny = False
+
+if temp >= 28 and is_sunny:
+    print("It is HOT outside")
+    print("It is SUNNY")
+elif temp <= 0 and is_sunny:
+    print("It is COLD outside")
+    print("Is is SUNNY")
+elif 28 > temp > 0 and is_sunny:
+    print("It is WARM outside")
+    print("It is SUNNY")
+if temp >= 28 and not is_sunny:
+    print("It is HOT outside")
+    print("It is CLOUDY")
+elif temp <= 0 and not is_sunny:
+    print("It is COLD outside")
+    print("Is is CLOUDY")
+elif 28 > temp > 0 and not is_sunny:
+    print("It is WARM outside")
+    print("It is CLOUDY")
---
+++ 12_conditional_expressions.py
@@ -0,0 +1,60 @@
+
+"""
+Positive or Negative
+
+num = -5
+print("Positive" if num > 0 else "Negative")
+
+"""
+
+"""
+Even or Odd
+
+num = 3
+result = "Even" if num % 2 == 0 else "Odd"
+print(result)
+
+"""
+
+"""
+Max number
+
+num = 5
+a = 6
+b = 7
+
+max_num = a if a > b else b
+print(max_num)
+
+"""
+"""
+Min Number
+num = 5
+a = 6
+b = 7
+
+min_num = a if a < b else b
+print(min_num)
+
+"""
+"""
+Adult of Child
+age = 25
+status = "Adult" if age >= 18 else "Child"
+
+print(status)
+"""
+"""
+Hot or Cold
+temperature = 30
+weather = "HOT" if temperature > 20 else "COLD"
+
+print(weather)
+"""
+
+# Admin or Limited Access
+
+user_role = "admin"
+access_level = "Full Access" if user_role == "admin" else "Limited Access"
+
+print(access_level)
