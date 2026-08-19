2f43e42d58146b9a32dcc7591fe8350bdce2a8c0
Demegorash <felipe.solisgranados@gmail.com>
2026-08-18 21:07:24 -0600

    Banking App Exercice completed

--- 41_if_name_main.py
+++ 41_if_name_main.py
@@ -7,3 +7,17 @@ if __name__ == '__main__':                        # dunder name or dunder main m
     main()             
     
 """
+
+# This is an example, we need to create 2 scripts, so we can call each one depending of the request.
+
+def favorite_food(food):
+    print(f"Your favorite food is {food}")
+    
+def main():
+    print("This is script1")
+    favorite_food("pizza")
+    print("Goodbye!")
+    
+
+if __name__ == '__main__':
+    main()
